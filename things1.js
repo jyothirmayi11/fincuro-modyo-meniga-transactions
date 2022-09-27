16098fd8853a9c336e9ed2decaad59d1369da1e3
jyothirmayi-fincuro <114382698+jyothirmayi-fincuro@users.noreply.github.com>
2022-09-27 15:04:28 +0530

    Create things1.js

---
+++ things1.js
@@ -0,0 +1,7 @@
+state = {
+    questions: [
+        { id: 'fdsd', title: 'Why is the maroon red?' },
+    { id: 'adsf' , title: ' Who ivented pizza?' },   
+    { id: 'afdsf', title: 'Is green tea overrated?' }, ]
+};
+displayQuestions: false
